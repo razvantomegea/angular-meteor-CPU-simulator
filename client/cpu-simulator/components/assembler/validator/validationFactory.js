38ca7cc692e3e9d4cc026d73a4a8dc1b4a876c2d
razvantomegea <razvan.tomegea@gmail.com>
2016-04-27 14:15:20 +0300

    Added comments in validation factory

--- client/cpu-simulator/components/assembler/validator/validationFactory.js
+++ client/cpu-simulator/components/assembler/validator/validationFactory.js
@@ -1,42 +1,95 @@
-/**
- * Created by razva on 4/10/2016.
+/*
+ *  This is the validation factory, where the entire validation functionality is implemented
  */
 (function (angular) {
 
     'use strict';
+    /**
+     * Use the factory returning object to validate the code
+     * using built-in methods
+     * @param  {object} $log               Angular console logging service
+     * @param  {object} instructionService Use this service to get the instruction definitions
+     * @return {object}                    Returns the validator with the validation built-in methods
+     */
     function validationFactory($log, instructionService) {
+
+        // Regular expresions definitions for the 4 instruction classes
+        // Match: ".label: INSTRUCTION OPERAND1,OPERAND2;
         const twoOperandClassRegex = /^(\.[a-z0-9]{1,9}:(\s|\t))?([A-Z]{2,4}\s((R[0-9]{1,2})|(\(R[0-9]{1,2}\))|[0-9]{1,4}?(\(R[0-9]{1,2}\))),((R[0-9]{1,2})|(\(R[0-9]{1,2}\))|[0-9]{1,4}?(\(R[0-9]{1,2}\))|((0x)?[0-9A-F]{1,4}?));)$/;
+        
+        // Match: ".label: INSTRUCTION OPERAND;
         const oneOperandClassRegexS = /^(\.[a-z0-9]{1,9}:(\s|\t))?([A-Z]{2,4}\s((R[0-9]{1,2})|(\(R[0-9]{1,2}\))|[0-9]{1,4}?(\(R[0-9]{1,2}\)));)$/;
+        
+        // Match: ".label: INSTRUCTION .label;
         const branchOperandClassRegex = /^(\.[a-z0-9]{1,9}:(\s|\t))?([A-Z]{2,4}\s(\.([a-z0-9]{1,9}));)$/;
+        
+        // Match: ".label: INSTRUCTION;
         const noOperandClassRegex = /^(\.[a-z0-9]{1,9}:(\s|\t))?((([A-Z]{2,4})|((PUSH|POP)\s(PC|FLAG)));)$/;
         
+        // Array that will contain the inserted instructions
+        // This array is used for validation and syntax verification purposes
+        let instructionSet = [];
+
+        // The validator object implementation 
         let validator = {
-            highMacroInstructionSet: [],
+
+            // Array of objects that contain information for each instruction
+            // It is used to ease the decodification process
+            highInstructionSet: [],
+
+            // Valid code flag that enables the assembly and execution
             validCode: false,
+
+            // Syntax error flag that saves the line of the error
+            // Indicates the line the syntax error comes from (not implemented yet)
             syntaxError: {
                 error: false,
                 line: 0
             },
+
+            // Invalid instruction flag
+            // Indicates the invalid instruction line, as well
             invalidInstruction: {
                 error: false,
                 line: 0
             },
+
+            // This method verifies that code syntax
             verifyCodeSyntax(code) {
+
+                // Assume the code is invalid
                 this.validCode = false;
-                let instructionSet = code.split("\n");
+
+                // Counter for good instruction syntax
                 let goodInstructionCount = 0;
-                angular.forEach(instructionSet, (item) => {
-                    if ((twoOperandClassRegex.test(item) === true) || (oneOperandClassRegexS.test(item) === true) || (noOperandClassRegex.test(item) === true) || (branchOperandClassRegex.test(item) === true)) {
+
+                // Get the instructions
+                instructionSet = code.split("\n");
+                
+                angular.forEach(instructionSet, item => {
+                    if (twoOperandClassRegex.test(item) || oneOperandClassRegexS.test(item) || noOperandClassRegex.test(item) || branchOperandClassRegex.test(item)) {
+                        
+                        // If the current iterated instruction is valid
+                        // inctement the counter
                         goodInstructionCount++;
                     }
+
+                    // If the counter is not equal to the number of instructions
+                    // there is a syntax error
                     this.syntaxError.error = (goodInstructionCount !== instructionSet.length);
                 });
             },
+
+            // This method validates the code
             validateCode(code) {
-                let instructionSet = code.split("\n");
+
+                // Counter for invalid instructions
                 let badInstructionCount = 0;
-                this.highMacroInstructionSet = [];
-                angular.forEach(instructionSet, (item) => {
+
+                // Clear the current instructionset
+                this.highInstructionSet = [];
+
+                angular.forEach(instructionSet, item => {
                     let currentInstruction = {
                         code: '',
                         class: 0
@@ -68,13 +121,14 @@
                     }
                     currentInstruction.code = item;
                     currentInstruction.class = classMatch;
-                    this.highMacroInstructionSet.push(currentInstruction);
+                    this.highInstructionSet.push(currentInstruction);
                     this.invalidInstruction.error = (badInstructionCount !== 0);
                 });
 
                 this.validCode = !this.invalidInstruction.error;
             }
         };
+
         return validator;
     }
     
